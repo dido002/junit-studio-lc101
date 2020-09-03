86d5ddec470d884811dcb4aebcd8149bd519ee86
Umer <an01985i@gmail.com>
2020-09-02 22:16:07 -0500

    final Update

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,56 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void bracketWithTextInMiddle(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode"));
+    }
+    @Test
+    public void nestedBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+    @Test
+    public void deepNestedBrackets(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[[] ]]]]]"));
+    }
+    @Test
+    public void justTextNoBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Here the code"));
+    }
+    @Test
+    public void bracketParenthesisBrackets(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[()"));
+    }
+    @Test
+    public void leftBrackets(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void wrongOrderBrackets(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+    @Test
+    public void missMatchBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch []"));
+    }
+    @Test
+    public void onlyLeftBrackets(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+    @Test
+    public void reverceBrackets(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void openBracketFollowedByTextNoClose(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
     }
 
 
-}
+
